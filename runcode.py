e7898bebbdea440b7e4ff640cabf23b6fb95827e
anhphant <anhphanuithanh@gmail.com>
2026-07-01 09:13:05 +0700

    ils04

--- runcode.py
+++ runcode.py
@@ -11,18 +11,11 @@ from launcherfunc import aco_launcher, brkga_launcher, ils_launcher, acopp_launc
 
 if __name__ == "__main__":
 
-    # tsp_base = ["d2103", "d15112", "pla7397"] # gg pla33810
-    # number_of_items_per_city = [1, 3, 5, 10, ]
-    # knapsack_type = ["bsc", "unc", "usw", ]
-    # knapsack_size = [1, 5, 10, ]
-    # maximum_travel_time = [1, 2, 3, ]
-    # number_of_runs = 1
-
-    tsp_base = ["pla33810"] # gg pla33810
-    number_of_items_per_city = [10]
-    knapsack_type = ["bsc" ]
-    knapsack_size = [10]
-    maximum_travel_time = [3]
+    tsp_base = ["d2103", "d15112", "pla7397"] # gg pla33810
+    number_of_items_per_city = [1, 3, 5, 10, ]
+    knapsack_type = ["bsc", "unc", "usw", ]
+    knapsack_size = [1, 5, 10, ]
+    maximum_travel_time = [1, 2, 3, ]
     number_of_runs = 1
 
     os.system("make clean")
